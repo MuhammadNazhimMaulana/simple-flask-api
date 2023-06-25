3b37c60fbb064f9e49eb24faa75496f7d0211292
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-06-25 13:43:05 +0700

    Adding Post Request

--- blueprints/API/first_api.py
+++ blueprints/API/first_api.py
@@ -1,6 +1,19 @@
-from flask import Response, request, jsonify
+from flask import Response, request, json
 from flask_restful import Resource
 
 class FakeApi(Resource):
+
+    # Get
     def get(self):
-        return Response("Berhasil", mimetype="application/json", status=200)
+        return Response(response=json.dumps({
+            "data": "Berhasil"
+        }), mimetype="application/json", status=200)
+    
+    # Post
+    def post(self):
+        # Get Requested data (Raw)
+        sent_data = request.get_json()
+
+        return Response(response=json.dumps({
+            "data": sent_data
+        }), mimetype="application/json", status=200)
